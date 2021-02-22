672ab5b4df35238e6c3f0255ca763e9436330fe0
minch <rusevstoyanov@gmail.com>
2021-02-22 11:39:32 -0600

    third change

--- src/main/java/stepDefinitons/Covid.java
+++ src/main/java/stepDefinitons/Covid.java
@@ -9,11 +9,18 @@ public class Covid {
 @Given("^User navigate to homePage$")
 public void user_navigate_to_homePage() throws Throwable {
     System.out.println("try and catch");
+    System.out.println("hello world");
+    System.out.println("hello asgfworld");
+    System.out.println("heasdfllo world");
+    System.out.println("hello wasgaorld");
+    System.out.println("hello world");
 }
 
 @When("^User click on topics button,dropdown menu should display$")
 public void user_click_on_topics_button_dropdown_menu_should_display() throws Throwable {
     System.out.println("hello world");
+    System.out.println("hello sfasforld");
+    System.out.println("hello woasfasfrld");
 }
 
 @Then("^User click on coronavirus button$")
